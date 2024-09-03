6ec67b009ef9c2c743cdc36031c00843de54c2f4
outanwang <outanwang@gmail.com>
2024-09-03 20:56:13 +0900

    S3バケット作成し,page.htmlをアップロードし、EC2からS3条のpage.htmlを表示

--- privatelink_aws/provider_vpc_stack.py
+++ privatelink_aws/provider_vpc_stack.py
@@ -3,9 +3,13 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_elasticloadbalancingv2 as elbv2,
     aws_iam as iam,
-    aws_elasticloadbalancingv2_targets as targets
+    aws_elasticloadbalancingv2_targets as targets,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    RemovalPolicy
 )
 from constructs import Construct
+import time
 
 class ProviderVpcStack(Stack):
 
@@ -63,24 +67,11 @@ class ProviderVpcStack(Stack):
             ]
         )
 
-        # EC2インスタンス作成
-        self.ec2_instance = ec2.Instance(self, "WebServer",
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-            machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
-            vpc=self.vpc,
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
-            security_group=self.web_sg,
-            role=self.ec2_role
-        )
 
-        # ユーザーデータスクリプトの追加
-        self.ec2_instance.add_user_data(
-            "yum update -y",
-            "yum install -y httpd",
-            "echo '<html><body><h1>Hello from Provider VPC!</h1></body></html>' > /var/www/html/index.html",
-            "systemctl start httpd",
-            "systemctl enable httpd"
-        )
+        # EC2インスタンスにS3アクセス権限を付与
+        self.ec2_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"))
+
+
 
         # NLB作成
         self.nlb = elbv2.NetworkLoadBalancer(self, "NLB",
@@ -99,7 +90,6 @@ class ProviderVpcStack(Stack):
                 protocol=elbv2.Protocol.HTTP
             )
         )
-        self.target_group.add_target(targets.InstanceTarget(self.ec2_instance))
 
         # リスナー作成
         self.nlb.add_listener("Listener",
@@ -107,10 +97,55 @@ class ProviderVpcStack(Stack):
             protocol=elbv2.Protocol.TCP,
             default_target_groups=[self.target_group]
         )
+        
 
         # VPCエンドポイントサービス作成
         self.endpoint_service = ec2.VpcEndpointService(self, "EndpointService",
             vpc_endpoint_service_load_balancers=[self.nlb],
             acceptance_required=False,
             allowed_principals=[iam.ArnPrincipal(self.ec2_role.role_arn)]
+        )
+
+        # S3バケットの作成
+        timestamp = int(time.time())
+        self.bucket = s3.Bucket(self, "ProviderVpcWebsiteBucket",
+            bucket_name=f"provider-vpc-website-bucket-{timestamp}",
+            public_read_access=False,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            removal_policy=RemovalPolicy.DESTROY,  # スタック削除時にバケットを削除
+            auto_delete_objects=True  # バケット内のオブジェクトも自動削除
+        )
+
+        # page.htmlの作成とアップロード
+        s3deploy.BucketDeployment(self, "DeployWebsite",
+            sources=[s3deploy.Source.asset("./website-content")],
+            destination_bucket=self.bucket
+        )
+
+        # S3用のVPCエンドポイントを作成
+        self.vpc.add_gateway_endpoint("S3Endpoint",
+            service=ec2.GatewayVpcEndpointAwsService.S3
+        )
+
+        # EC2インスタンス作成
+        self.ec2_instance = ec2.Instance(self, "WebServer",
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+            machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
+            vpc=self.vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
+            security_group=self.web_sg,
+            role=self.ec2_role
+        )
+
+        self.target_group.add_target(targets.InstanceTarget(self.ec2_instance))
+
+        # EC2インスタンスのユーザーデータを更新
+        self.ec2_instance.add_user_data(
+            "yum update -y",
+            "yum install -y httpd",
+            "chown -R ec2-user:apache /var/www/html",
+            f"aws s3 cp s3://{self.bucket.bucket_name}/page.html /var/www/html/index.html",
+            "systemctl start httpd",
+            "systemctl enable httpd"
         )
