3cad2033ad31149546756ae647a8e5563d2fe751
outanwang <outanwang@gmail.com>
2024-09-04 16:02:59 +0900

    S3から取得したindex.htmlを/var/www/htmlに保存できるように/var/www/htmlフォルダの権限を修正

--- privatelink_aws/provider_vpc_stack.py
+++ privatelink_aws/provider_vpc_stack.py
@@ -144,9 +144,8 @@ class ProviderVpcStack(Stack):
         self.ec2_instance.add_user_data(
             "yum update -y",
             "yum install -y httpd",
-            "chown -R /var/www/html",
             "sudo systemctl start httpd",
             "sudo systemctl enable httpd",
+            "sudo chmod -R 777 /var/www/html",
             f"aws s3 cp s3://{self.bucket.bucket_name}/page.html /var/www/html/index.html",
-
         )
